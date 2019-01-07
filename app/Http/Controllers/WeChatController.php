62e07eff83994bc4af528009398371813d20587a
EdisonK <zhd@thinkerx.com>
2019-01-07 10:26:22 +0800

    记录日志

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -8,7 +8,7 @@ class WeChatController extends Controller
 {
     public function serve()
     {
-        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+        \Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $app = app('wechat.official_account');
         $userApi = $app->user;
@@ -19,7 +19,7 @@ class WeChatController extends Controller
                     break;
                 case 'text':
                     $nickName = $userApi->get($message['FromUserName'])->nickname;
-                    Log::info($nickName);
+                    \Log::info($nickName);
 
                     return '收到文字消息';
                     break;
