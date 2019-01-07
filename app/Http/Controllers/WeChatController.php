0e53a76261d25321957de0327239440310e8c025
EdisonK <zhd@thinkerx.com>
2019-01-07 10:34:55 +0800

    记录日志

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use EasyWeChat\Factory;
 
 class WeChatController extends Controller
 {
@@ -10,7 +11,9 @@ class WeChatController extends Controller
     {
         \Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
-        $app = app('wechat.official_account');
+
+//        $app = app('wechat.official_account');
+        $app = Factory::officialAccount(config('wechat.official_account'));
         $userApi = $app->user;
         $app->server->push(function($message) use($userApi){
             \Log::info($message);
